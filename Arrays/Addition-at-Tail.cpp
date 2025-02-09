ddb436316f4195ea6ea9ae8ea5545d787327a36a
Mirza Shaheer Baig <shaheerbaig75@gmail.com>
2025-02-09 13:37:27 +0500

    Create Addition-at-Tail.cpp

---
+++ Arrays/Addition-at-Tail.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char array[10];  // Fixed-size array of 10 elements
+    int size = 5;    // Initially, the array has 5 elements
+
+    // Initialize the array with 5 elements, leaving the rest empty
+    array[0] = 'A';
+    array[1] = 'B';
+    array[2] = 'C';
+    array[3] = 'D';
+    array[4] = 'E';
+
+    // Display the initial array (5 elements filled)
+    cout << "Initial array: ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    // New element to be added at the tail
+    char newElement = 'Z';
+
+    // Check if the array has space to add a new element
+    if (size < 10) {
+        // Add the new element at the tail (index 'size')
+        array[size] = newElement;
+
+        // Increase the size of the array
+        size++;
+    } else {
+        cout << "Array is full, cannot add new element." << endl;
+    }
+
+    // Display the array after adding the new element at the tail
+    cout << "Array after adding new element at tail: ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
