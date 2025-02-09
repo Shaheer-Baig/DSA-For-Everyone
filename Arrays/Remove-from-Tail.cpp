e9da510c1e294d6d99314d5341582bdf5fa68b6c
Mirza Shaheer Baig <shaheerbaig75@gmail.com>
2025-02-09 13:38:45 +0500

    Create Remove-from-Tail.cpp

---
+++ Arrays/Remove-from-Tail.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char array[10];  // Fixed-size array of 10 elements
+    int size = 5;    // Initially, the array has 5 elements
+
+    // Initialize the array with 5 elements
+    array[0] = 'A';
+    array[1] = 'B';
+    array[2] = 'C';
+    array[3] = 'D';
+    array[4] = 'E';
+
+    // Display the initial array
+    cout << "Initial array: ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    // Remove the element at the tail (last element)
+    if (size > 0) {
+        // Simply reduce the size of the array
+        size--;
+    } else {
+        cout << "Array is empty, cannot remove element." << endl;
+    }
+
+    // Display the array after removing the element from the tail
+    cout << "Array after removing element from tail: ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
