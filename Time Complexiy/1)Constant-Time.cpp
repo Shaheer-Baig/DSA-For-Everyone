b67ed366f32c714b19e6e8379f382880243cc619
Mirza Shaheer Baig <shaheerbaig75@gmail.com>
2025-02-09 13:09:10 +0500

    Update 1)0(1)Constant-Time.cpp

--- Time Complexiy/1)Constant-Time.cpp
+++ Time Complexiy/1)Constant-Time.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int getFirstElement(int arr[], int n) {
+    return arr[0]; 
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "First Element: " << getFirstElement(arr, n) << endl;
+    return 0;
+}
 
